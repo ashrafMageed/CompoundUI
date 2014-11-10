2ee00ae4558992335fc0c0de1f75bc2fe35d1e5b
ashrafmageed <ashraf_mageed@hotmail.com>
2014-11-10 18:18:58 +0000

    Made services html endpoint more dynamic

--- CompoundUI.TestEndpoint/Controllers/CasesViewController.cs
+++ CompoundUI.TestEndpoint/Controllers/CasesViewController.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Design.Serialization;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Web.Http;
 
@@ -10,13 +11,18 @@ namespace CompoundUI.TestEndpoint.Controllers
 {
     public class CasesViewController : ApiController
     {
-        [Route("CasesView/{relativePath}")]
-        public HttpResponseMessage Get(string relativePath)
+        [Route("CasesView/{viewName}")]
+        public HttpResponseMessage Get(string viewName)
         {
+            //File.ReadAllText(String.Format(@"{0}\{1}.html", pagesDirectory, pageName));
+            var applicationPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            var pagesDirectory = Path.Combine(applicationPath, @"Content\");
             var pageStringBuilder = new StringBuilder();
-            pageStringBuilder.Append(EncloseInScriptTags(File.ReadAllText(@"C:\Users\mageas\Documents\GitHub\CompoundUI\CompoundUI.TestEndpoint\Content\Controllers\CaseDetailsController.js")));
-            pageStringBuilder.Append(EncloseInScriptTags(File.ReadAllText(@"C:\Users\mageas\Documents\GitHub\CompoundUI\CompoundUI.TestEndpoint\Content\Services\CasesService.js")));
-            pageStringBuilder.Append(File.ReadAllText(@"C:\Users\mageas\Documents\GitHub\CompoundUI\CompoundUI.TestEndpoint\Content\Views\CaseDetails.html"));
+            pageStringBuilder.Append(EncloseInScriptTags(File.ReadAllText(String.Format(@"{0}\Controllers\{1}Controller.js", pagesDirectory, viewName))));
+            pageStringBuilder.Append(EncloseInScriptTags(File.ReadAllText(String.Format(@"{0}\Services\CasesService.js", pagesDirectory))));
+            pageStringBuilder.Append(File.ReadAllText(String.Format(@"{0}\Views\{1}.html", pagesDirectory, viewName)));
+//            pageStringBuilder.Append(EncloseInScriptTags(File.ReadAllText(@"C:\Users\mageas\Documents\GitHub\CompoundUI\CompoundUI.TestEndpoint\Content\Services\CasesService.js")));
+//            pageStringBuilder.Append(File.ReadAllText(@"C:\Users\mageas\Documents\GitHub\CompoundUI\CompoundUI.TestEndpoint\Content\Views\CaseDetails.html"));
             var responseBody = pageStringBuilder.ToString();
             var response = Request.CreateResponse(System.Net.HttpStatusCode.OK, responseBody, new TextPlainFormatter());
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-javascript");
